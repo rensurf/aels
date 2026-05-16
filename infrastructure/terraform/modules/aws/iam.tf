bfb77163fb1f63c84db5b562271c79f55e940f82
ren-surf <ren.nakamura.engineer@gmail.com>
2026-05-16 22:05:59 +1000

    fix: add DynamoDB permissions to Lambda IAM role

--- infrastructure/terraform/modules/aws/iam.tf
+++ infrastructure/terraform/modules/aws/iam.tf
@@ -15,3 +15,21 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy" "lambda_dynamodb" {
+  name = "aels-lambda-dynamodb"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem"
+      ]
+      Resource = "arn:aws:dynamodb:ap-southeast-2:813668016503:table/aels-sessions"
+    }]
+  })
+}
